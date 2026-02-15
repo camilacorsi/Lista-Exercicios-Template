960ce20c5d81f65ec0641769dc0c2eed9f2bb3df
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:29:45 -0300

    Update Program.cs

--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 5) Conversão de metros para centímetros
+//Peça um valor em metros e mostre o valor em centímetros.
+//(1 metro = 100 cm)
+Console.WriteLine("Digite um valor em metros: ");
+var metro = Console.ReadLine();
+var metroConvert = double.Parse(metro);
+var centim = metroConvert * 100;
+var centimConvert = int.Parse(centim);
+var mensagem = "O valor dessa medida em centímetros é: " + centimConvert;
+Console.WriteLine(mensagem);
