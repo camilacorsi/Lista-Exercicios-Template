4329a02f4b58984db06b3d8f1e6abd15ce875d41
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:32:12 -0300

    Update Program.cs

--- Exercicio10/Program.cs
+++ Exercicio10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 10) Salário mensal
+//Peça:
+//- valor por hora
+//- horas trabalhadas no mês
+//Calcule e mostre salário final.
+Console.WriteLine("Quanto você ganha por hora: ");
+var valor = Console.ReadLine();
+var valorConvert = double.Parse(valor);
+Console.WriteLine("Quanta horas você trabalhou no mês: ");
+var hora = Console.ReadLine();
+var horaConvert = double.Parse(hora);
+var salFinal = valorConvert * horaConvert;
+Console.WriteLine("Seu salário final foi de: R$ " + salFinal);
