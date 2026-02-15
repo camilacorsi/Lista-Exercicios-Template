61740b90accf56548b31aec8e0b015c9dda9fbbe
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:32:38 -0300

    Update Program.cs

--- Exercicio11/Program.cs
+++ Exercicio11/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 11) Troco
+//Peça:
+//- valor do produto
+//- valor pago
+//Mostre:
+//- valor do troco
+Console.WriteLine("Digite o valor do produto: ");
+var produto = Console.ReadLine();
+var produtoConvert = double.Parse(produto);
+Console.WriteLine("Digite o valor pago: ");
+var pago = Console.ReadLine();
+var pagoConvert = double.Parse(pago);
+var troco = pagoConvert - produtoConvert;
+Console.WriteLine("O valor do troco é de R$ " + troco);
