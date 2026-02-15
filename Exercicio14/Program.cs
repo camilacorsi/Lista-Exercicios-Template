d73a9d20b43eed5b0c3dd4b44350a35c11d17aa9
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:33:49 -0300

    Update Program.cs

--- Exercicio14/Program.cs
+++ Exercicio14/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 14) Minicalculadora com método
+//Crie um método:
+//int Somar(int a, int b)
+//No programa principal:
+//- leia dois números
+//- chame o método
+//- mostre resultado
+
+Console.WriteLine("Digite um numero: ");
+var a = Somar();
+
+Console.WriteLine("Digite outro numero: ");
+var b = Somar();
+
+Console.WriteLine("A soma dos valores é igual a " + mensagem);
+
+static int Somar()
+{
+    int a = Console.ReadLine();
+    var aConvert = int.Parse(a);
+
+    int b = Console.ReadLine();
+    var bConvert = int.Parse(b);
+
+    int soma = aConvert + bConvert;
+
+    var mensagem = soma;
+    return mensagem;
+
+}
