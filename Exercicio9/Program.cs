049595451e7ccc16a734eb997b1bee620cbfda32
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:31:44 -0300

    Update Program.cs

--- Exercicio9/Program.cs
+++ Exercicio9/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 9) Divisão segura
+//Peça dois números e faça a divisão.
+//Tratar:
+//- erro de conversão
+//- divisão por zero
+//Mostrar mensagens apropriadas.
+try
+{
+    Console.WriteLine("Digite um número: ");
+    var n1 = Console.ReadLine();
+    var n1Convert = int.Parse(n1);
+
+    Console.WriteLine("Digite outro número: ");
+    var n2 = Console.ReadLine();
+    var n2Convert = int.Parse(n2);
+
+    var div = n1Convert / n2Convert;
+    Console.WriteLine(div);
+}
+
+catch (FormatException)
+{
+    Console.WriteLine("erro de conversão");
+}
+
+catch(System.DivideByZeroException)
+{ 
+    Console.WriteLine("denominador não pode ser zero");
+}
