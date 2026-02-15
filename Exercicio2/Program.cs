8538bf1056d18f019cc2144b9fcbb086d2cd34f5
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:27:37 -0300

    Update Program.cs

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 2) Antecessor e sucessor
+//Peça um número inteiro e mostre:
+//- antecessor
+//- sucessor
+//Ex:
+//Digite um número: 10
+//Antecessor: 9
+//Sucessor: 11
+Console.WriteLine("Digite um número: ");
+var numero = Console.ReadLine();
+var numConvert = int.Parse(numero);
+var antecessor = numConvert - 1;
+var sucessor = numConvert + 1;
+var mensagem = "O número antecessor é: " + antecessor;
+var mensagem2 = "O número sucessor é: " + sucessor;
+Console.WriteLine(mensagem);
+Console.WriteLine(mensagem2);
