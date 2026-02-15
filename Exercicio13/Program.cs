e231ff165e3115667e92e44198328b65afa451c2
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:33:24 -0300

    Update Program.cs

--- Exercicio13/Program.cs
+++ Exercicio13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 13) Média de duas provas
+//Peça duas notas (double).
+//Mostre média com 2 casas decimais.
+//Dica:
+//Use ToString("F2")
+Console.WriteLine("Digite a primeira nota: ");
+var n1 = Console.ReadLine();
+var n1Convert = double.Parse(n1);
+Console.WriteLine("Digite a segunda nota: ");
+var n2 = Console.ReadLine();
+var n2Convert = double.Parse(n2);
+var media = (n1Convert + n2Convert)/2;
+var mediaConv = Convert.ToString(media);
+Console.WriteLine("Sua média foi de: " + mediaConv);
