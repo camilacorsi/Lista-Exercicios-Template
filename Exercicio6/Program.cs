7c799179d204d28a5e790595f06f0879b8d6242d
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:30:08 -0300

    Update Program.cs

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 6) Conversão de temperatura
+//Peça uma temperatura em Celsius e converta para Fahrenheit.
+//Fórmula:
+//F = (C × 9 / 5) +32
+Console.WriteLine("Digite a temperatura em graus Celsius: ");
+var celsius = Console.ReadLine();
+var celsiusConvert = double.Parse(celsius);
+var far = (celsiusConvert * 9 / 5) + 32;
+var mensagem = "O valor dessa temperatura em graus Fahrenheit é: " + far;
+Console.WriteLine(mensagem);
