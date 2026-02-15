c46ee6802e406eb873e0be8cf9cd06df731a3b5b
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:30:46 -0300

    Update Program.cs

--- Exercicio7/Program.cs
+++ Exercicio7/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 7) Leitura segura de número
+//Peça um número inteiro.
+//Use try/catch .
+//Se o usuário digitar texto:
+//Mostrar: "Entrada inválida. Digite apenas números."
+try
+{
+    Console.WriteLine("Digite um número inteiro: ");
+    var numero = Console.ReadLine();
+    var numConvert = int.Parse(numero);
+    Console.WriteLine(numConvert);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Entrada inválida. Digite apenas números");
+}
