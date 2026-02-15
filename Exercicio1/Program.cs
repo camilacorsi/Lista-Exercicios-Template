bf1157cdc6a7b32a7bcd9526ef85935c8dc16603
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:27:08 -0300

    Update Program.cs

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 1) Dobro de um número
+//Peça um número inteiro ao usuário e mostre:
+//- o número digitado
+//- o dobro desse número
+Console.WriteLine("Digite um número: ");
+var numero = Console.ReadLine();
+var numConvert = int.Parse(numero);
+var dobro = numConvert * 2;
+var mensagem = "O dobro desse número é: " +  dobro;
+Console.WriteLine(mensagem);
