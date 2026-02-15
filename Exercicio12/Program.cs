ec07b29f81efa539215757bf0ba96b8aa23c5215
Camila Corsi <camilacorsi6@gmail.com>
2026-02-15 20:33:02 -0300

    Update Program.cs

--- Exercicio12/Program.cs
+++ Exercicio12/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercicio 12) Área do retângulo
+//Peça base e altura.
+//Mostre:
+//- área
+//- perímetro
+Console.WriteLine("Digite a base do retângulo: ");
+var baseret = Console.ReadLine();
+var baseConvert = double.Parse(baseret);
+
+Console.WriteLine("Digite a altura do retângulo: ");
+var altura = Console.ReadLine();
+var alturaConvert = double.Parse(altura);
+
+var area = baseConvert * alturaConvert;
+Console.WriteLine("A área do retângulo é de : " + area);
+
+var perimetro = 2 * area;
+Console.WriteLine("O perímetro do retângulo é de : " + perimetro);
